347c8c761fa6886185c5407644582b05b5de799b
JonnyJiang123 <JonnyJiang219@gmail.com>
2024-12-07 14:02:42 +0800

    SERVER优化

--- server/src/main/java/cn/mj/thrift/test/server/service/HelloServiceImpl.java
+++ server/src/main/java/cn/mj/thrift/test/server/service/HelloServiceImpl.java
@@ -9,11 +9,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.thrift.TException;
 import org.apache.thrift.async.AsyncMethodCallback;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
 @Slf4j
 @Service("helloService")
 public class HelloServiceImpl implements HelloService.AsyncIface {
     @Resource
     private ObjectMapper objectMapper;
+    private final Random random = new Random();
 
     @Override
     public void sayHello(HelloRequest req, AsyncMethodCallback<HelloResponse> resultHandler) throws TException {
@@ -21,10 +26,14 @@ public class HelloServiceImpl implements HelloService.AsyncIface {
         try{
             var json = objectMapper.writeValueAsString(req);
 //            log.info("sayHello: {}", json);
+
             helloResponse.setAge(19);
             helloResponse.setName(req.getName());
             helloResponse.setMessage("hello, " + req.getName());
             helloResponse.setTimestamp(System.currentTimeMillis());
+
+            int sleepTime = random.nextInt(100, 200);
+//            TimeUnit.MILLISECONDS.sleep(sleepTime);
         }catch (Exception e){
             log.error("sayHello failed: ", e);
             helloResponse.setMessage("error, " +e.getMessage());
--- server/src/main/java/cn/mj/thrift/test/server/service/ThriftServer.java
+++ server/src/main/java/cn/mj/thrift/test/server/service/ThriftServer.java
@@ -6,7 +6,10 @@ import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.thrift.server.TNonblockingServer;
 import org.apache.thrift.server.TServer;
+import org.apache.thrift.server.TThreadPoolServer;
+import org.apache.thrift.server.TThreadedSelectorServer;
 import org.apache.thrift.transport.TNonblockingServerSocket;
+import org.apache.thrift.transport.TServerSocket;
 import org.apache.thrift.transport.TTransportException;
 import org.springframework.stereotype.Component;
 
@@ -18,16 +21,27 @@ public class ThriftServer {
     @PostConstruct
     public void init() throws TTransportException {
         log.info("ThriftServer init");
+//        new Thread(()->{
+//            try(TNonblockingServerSocket serverTransport = new TNonblockingServerSocket(9999)){
+//                TServer server = new TNonblockingServer(new TNonblockingServer.Args(serverTransport)
+//                        .processor(new HelloService.AsyncProcessor<>(helloService)));
+//                server.serve();
+//            }catch (Exception e){
+//                log.error("init failed: ", e);
+//            }
+//
+//        }).start();
         new Thread(()->{
             try(TNonblockingServerSocket serverTransport = new TNonblockingServerSocket(9999)){
-                TServer server = new TNonblockingServer(new TNonblockingServer.Args(serverTransport)
-                        .processor(new HelloService.AsyncProcessor<>(helloService)));
+                TServer server = new TThreadedSelectorServer(new TThreadedSelectorServer.Args(serverTransport)
+                        .processor(new HelloService.AsyncProcessor<>(helloService))
+                        .selectorThreads(10).workerThreads(200));
                 server.serve();
             }catch (Exception e){
                 log.error("init failed: ", e);
             }
-
+            log.info("ThriftServer init done");
         }).start();
-        log.info("ThriftServer init done");
+
     }
 }
